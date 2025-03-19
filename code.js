b4c3e15aa32d601795aaa3f345075af99eb88082
caiolaurenco <caio_laurenco@estudante.sesisenai.org.br>
2025-03-19 09:48:25 -0300

    ADD - começei o código

---
+++ code.js
@@ -0,0 +1,11 @@
+function verificarPar(numero){
+    if(numero % 2 === 0 ){
+        console.log = ("o número é par");
+        }else{
+        console.log = ("o número é impar");
+        }   
+}
+  
+let numero = parseInt (prompt("digite um numero"));
+verificarPar(numero);
+
