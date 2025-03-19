912adf0052a57c8f203b976af411f8a94c8552f5
caiolaurenco <caio_laurenco@estudante.sesisenai.org.br>
2025-03-19 10:01:04 -0300

    UPDATE - atividade numero 2

--- code.js
+++ code.js
@@ -1,11 +1,16 @@
-function verificarPar(numero){
-    if(numero % 2 === 0 ){
-        console.log = ("o número é par");
-        }else{
-        console.log = ("o número é impar");
-        }   
+function calcular (numero1, numero2){
+    const soma = numero1 + numero 2;
+    const subtracao = numero1 + numero2;
+    const divisao = numero2 !== 0 ? numero1 / numero2 : 'não pode dividir por zero';
+    const multiplicacao = numero1 * numero 2;
+    
+    console.log(soma);
+    console.log(subtracao);
+    console.log(divisao);
+    console.log(multiplicacao);
 }
-  
-let numero = parseInt (prompt("digite um numero"));
-verificarPar(numero);
 
+let numero1 = parseFloat(prompt("digite seu primeiro número"));
+let numero2 = parseFloat(prompt("digite seu primeiro número"));
+
+calcular(numero1,numero2);
