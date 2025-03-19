1c8518d3d4074ba4c086d394df9fefc82b6fc7ea
caiolaurenco <caio_laurenco@estudante.sesisenai.org.br>
2025-03-19 10:08:57 -0300

    UPDATE - exercicio 3

--- code.js
+++ code.js
@@ -1,16 +1,7 @@
-function calcular (numero1, numero2){
-    const soma = numero1 + numero 2;
-    const subtracao = numero1 + numero2;
-    const divisao = numero2 !== 0 ? numero1 / numero2 : 'não pode dividir por zero';
-    const multiplicacao = numero1 * numero 2;
-    
-    console.log(soma);
-    console.log(subtracao);
-    console.log(divisao);
-    console.log(multiplicacao);
-}
+let numero1 = 10;
+let delay = 0;
 
-let numero1 = parseFloat(prompt("digite seu primeiro número"));
-let numero2 = parseFloat(prompt("digite seu primeiro número"));
-
-calcular(numero1,numero2);
+while(numero1 >=1){
+   console.log(numero1);
+   numero1--
+     }
