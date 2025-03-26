8cb587e6fa38e534edf046dbfa96bc8d1a8836ea
caiolaurenco <caio_laurenco@estudante.sesisenai.org.br>
2025-03-26 09:28:20 -0300

    UPDATE - exercicio 11

--- code.js
+++ code.js
@@ -1,7 +1,10 @@
-function palindroma(str){
-  return true;
+let numero = prompt();
 
+if (numero > 0){
+  console.log("o numero é positivo")
+} else if(numero == 0 ){
+  console.log("o numero é 0 ")
+}else{
+  console.log("o numero é negativo")
 }
-  console.log(palindroma("arara"))
-
 
